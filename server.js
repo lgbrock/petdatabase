004d6796fb5d1e4bd618c20e5358f91e33707910
Logan Brock <logan.g.brock@gmail.com>
2021-07-10 12:37:32 -0400

    initial commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./schema.js');
+const cors = require('cors');
+
+const app = express();
+
+// enable `cors` to set HTTP response header: Access-Control-Allow-Origin: *
+app.use(cors());
+
+app.use(
+	'/graphql',
+	graphqlHTTP({
+		schema: schema,
+		graphiql: true,
+	})
+);
+
+app.listen(4000, () => {
+	console.log('Server is running on port 4000..');
+});
